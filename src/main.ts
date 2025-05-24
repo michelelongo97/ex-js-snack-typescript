f3c87c53446b5609409186cfc913090d0920b4e1
michelelongo97 <m.longo7x@gmail.com>
2025-05-24 15:37:58 +0200

    feat : add snack 2 + bonus

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <h1>Snack TypeScript</h1>
   </div>
 `;
+
+// Snack 1
 //Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
 //Se è una stringa: stampala in maiuscolo
 //Se è un numero: moltiplicalo per due e stampalo
@@ -37,3 +39,25 @@ if (typeof dato === "string") {
 } else {
   console.log("Tipo non supportato");
 }
+
+//Snack 2
+//Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
+//nome → stringa
+//cognome → stringa
+//annoNascita → numero
+//sesso → Può essere solo "m" o "f".
+//anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+//🎯 BONUS
+//Il type alias Dipendente, ha anche i seguenti dati:
+//emailAziendale → Email assegnata al dipendente (non si può modificare)
+//contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string;
+  congnome: string;
+  annoNascita: number;
+  sesso: "m" | "f";
+  anniDiServizio: number[];
+  readonly emailAziendale: string;
+  contratto: "indeterminato" | "determinato" | "freelance";
+};
