ddbcd5322f1e68ed9898042a99c48d1955ee6ba1
michelelongo97 <m.longo7x@gmail.com>
2025-05-24 15:19:21 +0200

    feat: add snack 1

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,24 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <h1>Snack TypeScript</h1>
   </div>
 `;
+//Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+//Se è una stringa: stampala in maiuscolo
+//Se è un numero: moltiplicalo per due e stampalo
+//Se è un booleano: stampa “Sì” o “No” in base al suo valore
+//In tutti gli altri casi: stampa “Tipo non supportato”
+//🎯 BONUS
+//Se è null: stampa “Il dato è vuoto”
+//Se è un array: stampa la sua lunghezza
+//Se è una Promise: attendi che si risolva e stampa il valore del resolve.
+
+let dato: unknown;
+dato = [];
+if (typeof dato === "string") {
+  console.log(dato.toUpperCase());
+} else if (typeof dato === "number") {
+  console.log(dato * 2);
+} else if (typeof dato === "boolean") {
+  console.log(dato);
+} else {
+  console.log("Tipo non supportato");
+}
