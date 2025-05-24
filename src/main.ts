884e31b47fa713b813b747aef6300d7e6ef7d97d
michelelongo97 <m.longo7x@gmail.com>
2025-05-24 15:03:18 +0200

    init project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+   
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Snack TypeScript</h1>
+  </div>
+`;
